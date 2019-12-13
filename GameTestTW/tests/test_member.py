a54b656b7f67460f6b4157b2bed11c0f80168c27
zhuoyan <18108347985@163.com>
2019-12-13 14:59:06 +0800

    111

--- GameTestTW/tests/test_member.py
+++ GameTestTW/tests/test_member.py
@@ -5,8 +5,12 @@
 
 
 import json, time
-import sys,os
-sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+import sys
+import os
+# sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 from utils.isAssert import IsAssert
 from base.user import User
 from utils.public import *
